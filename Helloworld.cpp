76db660466c073d261e2e680127bccb4e905a6af
AngieN7 <117302924+AngieN7@users.noreply.github.com>
2023-09-17 20:51:20 -0400

    Add files via upload
    Hello world in C++.

---
+++ Helloworld.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+/*Main function where execution always begins.
+Standard for it to be delcared int, as int is the required return type for C++ functions.*/
+int main() {
+//Cout is function of iosstream library, uses to display output on screen, much like printf in C.
+  cout << "Hello World!";
+  return 0;
+}
